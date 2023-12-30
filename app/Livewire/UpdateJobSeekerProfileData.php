4c531b186d4a69089b190ea033954b5a22b0fb6f
jawad-elhajjami <jawadelhajjamiofficiel@gmail.com>
2023-12-30 18:55:13 +0100

    Add skill functionnality + dark mode for job seeker post page

--- app/Livewire/UpdateJobSeekerProfileData.php
+++ app/Livewire/UpdateJobSeekerProfileData.php
@@ -5,6 +5,8 @@ namespace App\Livewire;
 use Livewire\Component;
 use Livewire\Attributes\Validate;
 use App\Models\Candidate;
+use App\Models\Skills;
+use Illuminate\Support\Facades\Storage;
 use Livewire\WithFileUploads;
 
 class UpdateJobSeekerProfileData extends Component
@@ -12,81 +14,128 @@ class UpdateJobSeekerProfileData extends Component
     use WithFileUploads;
 
     public $user;
-    public $age;
     public $gender;
-    #[Validate('max:2048')]
-    public $resume;
-    public $resumeUrl;
-    public $changedResumeFile = false;
-    public $path;
-
-    // update validation rules based on the changedResumeFile boolean ; if the value of this varibale is true meaning the user changed the file it adds a validation rule for the file.
-    protected function rules() {
-        if ($this->resumeUrl == null && $this->changedResumeFile)
-            return [
-                'age' => 'numeric',
-                'resume' => 'file|mimes:pdf,docx|max:2048',
-            ];
-        else if($this->changedResumeFile)
-            return [
-                'age' => 'numeric',
-                'resume' => 'file|mimes:pdf,docx|max:2048',
-            ];
-        else
-            return [
-                'age' => 'numeric',
-            ];
-    }
-    // track the state of the changedResumeFile variable
-    public function updatedResume(){
-            if($this->changedResumeFile == false) $this->changedResumeFile = true;
-    }
+    public $resume, $resumeUrl, $resumePath;
+    public $coverPictureUrl, $coverPicturePath, $coverPicture;
+    public $coverLetterUrl, $coverLetterPath, $coverLetter;
+    public $skillsList;
+    public $showingAddSkillModal = false;
+    public $skills;
+    public $title;
+    public $age;
 
     public function mount()
     {
         $this->user = Candidate::where('user_id', auth()->user()->id)->first();
         $this->age = $this->user->age;
         $this->gender = $this->user->gender;
-        $this->resume = $this->user->curriculumVitae;
         $this->resumeUrl = $this->user->curriculumVitae;
+        $this->coverPictureUrl = $this->user->coverPicture;
+        $this->coverLetterUrl = $this->user->coverLetter;
+        $this->skillsList = Skills::all();
+        $this->skills = $this->user->skills;
     }
 
+
+    public function showAddSkillModal(){
+        $this->showingAddSkillModal = true;
+    }
+
+    // function to remove CV
     public function removeCV(){
+        Storage::delete($this->resumeUrl);
         $this->user->update([
             'curriculumVitae' => null,
         ]);
-
         // Redirect to the profile page
         return redirect('/user/profile');
     }
 
+    // function to remove cover picture
+    public function removeCoverPicture(){
+        Storage::delete($this->coverPictureUrl);
+        $this->user->update([
+            'coverPicture' => null
+        ]);
+        // Redirect to the profile page
+        return redirect('/user/profile');
+    }
+
+    // function to remove cover letter 
+    public function removeCoverLetter(){
+        Storage::delete($this->coverLetterUrl);
+        $this->user->update([
+            'coverLetter' => null
+        ]);
+        // Redirect to the profile page
+        return redirect('/user/profile');
+    }
+
+    public function addSkill(){
+        $validated = $this->validate([
+            'title' => 'required|max:25|unique:skills'
+        ]);
+        Skills::create([
+            'title' => $this->title
+        ]);
+        
+        $this->reset(['showingAddSkillModal']);
+
+        return redirect('/user/profile');
+
+    }
+
+    // function to save data to the DB
     public function save()
     {   
-        $validated = $this->validate($this->rules());
-        if ($this->resume && $this->changedResumeFile) {
-            $this->path = $this->resume->storeAs('public/uploads', 'cv-user'.auth()->user()->id.'.pdf');
-            if($this->changedResumeFile){
-                $this->user->update([
-                    'age' => $this->age,
-                    'curriculumVitae' => $this->path,
-                    'gender' => $this->gender,
-                ]);
-            }
-        }
-        else{
+        $validated = $this->validate([
+            'age' => 'numeric|max:50',
+            'gender' => 'nullable',
+            'resume' => 'nullable|file|mimes:pdf,docx|max:2048',
+            'coverPicture' => 'nullable|file|mimes:png,jpg,jpeg|max:2048',
+            'coverLetter' => 'nullable|file|mimes:pdf,docx|max:2048'
+        ]);
+        
+        $this->user->update([
+            'age' => $this->age,
+            'gender' => $this->gender,
+            'skills' => $this->skills
+        ]);
+        if ($this->resume) {
+            // storing file in storage/public/uploads folder
+            $this->resumePath = $this->resume->storeAs('public/uploads', 'cv-user'.auth()->user()->id.'.pdf');
+            
             $this->user->update([
-                'age' => $this->age,
-                'gender' => $this->gender,
+                'curriculumVitae' => $this->resumePath,
+            ]);
+        }
+
+        if($this->coverPicture){
+            $this->coverPicturePath = $this->coverPicture->storeAs('public/uploads/cover_pictures','cover-user'.auth()->user()->id.'.jpg');
+            
+            $this->user->update([
+                'coverPicture' => $this->coverPicturePath,
+            ]);
+        }
+
+        if($this->coverLetter){
+            $this->coverLetterPath = $this->coverLetter->storeAs('public/uploads/cover_letters','coverletter-user'.auth()->user()->id.'.pdf');
+            
+            $this->user->update([
+                'coverLetter' => $this->coverLetterPath,
             ]);
         }
 
         session()->flash('success', 'Data saved successfully !');
+
         // Redirect to the profile page
         return redirect('/user/profile');
     }
 
     public function render()
     {
-        return view('livewire.update-job-seeker-profile-data');
+        return view('livewire.update-job-seeker-profile-data',[
+            'skillsList' => Skills::all(),        
+        ]);
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,10 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+        
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
